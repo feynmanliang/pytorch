7c09387f1d93972de6b7ce59f3c278c68e9ebdae
Shen Li <cs.shenli@gmail.com>
2021-01-11 11:07:55 -0800

    Enable GPU-to-GPU comm in TensorPipeAgent
    This commit uses TensorPipe's cuda_ipc channel to conduct
    cross-process same-machine GPU-to-GPU communication. On the sender
    side, `TensorPipeAgent` grabs a stream to each device, let these
    streams wait for current streams, and passes the streams to
    TensorPipe `CudaBuffer`. On the receiver side, it also grabs a stream
    for each device, and uses these streams to receive tensors and run
    user functions. After that, these streams are then used for sending
    the response back to the sender. When receiving the response, the
    sender will grab a new set of streams and use them for TensorPipe's
    `CudaBuffer`.
    
    If device maps are provided, `TensorPipeAgent::send` will return a
    derived class of `CUDAFuture`, which is specifically tailored for
    RPC Messages.
    
    TODOs:
    1. When sending a request, RPC caller only need to grab streams for
    used tensor devices in the message, which is different from the callee.
    2. Enable sending CUDA RPC to the same process.
    3. Add a custom CUDA stream pool.
    
    ghstack-source-id: 990cae52569ed021a78200daa669deb0398083a6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44418

--- torch/csrc/distributed/rpc/tensorpipe_utils.h
+++ torch/csrc/distributed/rpc/tensorpipe_utils.h
@@ -2,7 +2,15 @@
 
 #ifdef USE_TENSORPIPE
 
+#include <torch/csrc/distributed/rpc/macros.h>
 #include <torch/csrc/distributed/rpc/utils.h>
+
+#ifdef USE_CUDA_NOT_ROCM
+#include <ATen/cuda/CUDAEvent.h>
+#include <c10/cuda/CUDAFunctions.h>
+#include <c10/cuda/CUDAStream.h>
+#include <c10/cuda/CUDACachingAllocator.h>
+#endif
 
 namespace tensorpipe {
 class Message;
@@ -11,6 +19,85 @@ class Message;
 namespace torch {
 namespace distributed {
 namespace rpc {
+
+#ifdef USE_CUDA_NOT_ROCM
+using at::cuda::CUDAStream;
+#endif
+
+
+// A general device context class for both CPU and CUDA. If CUDA is not
+// available, all CUDA-related methods will be no-ops.
+struct FullDeviceContext {
+
+  FullDeviceContext(const FullDeviceContext& other) = delete;
+  FullDeviceContext(FullDeviceContext&& other) = delete;
+
+  FullDeviceContext& operator=(const FullDeviceContext& rhs) = delete;
+  FullDeviceContext& operator=(FullDeviceContext&& rhs) & = delete;
+
+  explicit FullDeviceContext(bool noCuda) {}
+  virtual void recordDataPtrs(
+      const std::vector<c10::DataPtr>& dataPtrs) const {}
+  virtual void recordTensors(const std::vector<torch::Tensor>& tensors) const {}
+  virtual void blockCurrentStreams() const {}
+  virtual void waitForCurrentStreams() const {}
+
+#ifdef USE_CUDA_NOT_ROCM
+  virtual const std::vector<CUDAStream>& streams() const {
+    throw std::runtime_error(
+        "Attempting to access CUDA streams, but torch is not built with CUDA");
+  }
+#endif
+
+};
+
+#ifndef USE_CUDA_NOT_ROCM
+
+// CUDA is not available, use CPU device context.
+inline std::shared_ptr<FullDeviceContext> createFullDeviceContext(bool noCuda) {
+  return std::make_shared<FullDeviceContext>(noCuda);
+}
+
+#else
+
+// CUDA is available. Implement CUDA-related operations.
+struct CudaFullDeviceContext : public FullDeviceContext {
+
+  // Use the noCuda arg to disable streams management when deviceMaps are not
+  // set.
+  explicit CudaFullDeviceContext(bool noCuda) : FullDeviceContext(noCuda) {
+    if (!noCuda) {
+      auto deviceNum = at::cuda::device_count();
+      streams_.reserve(deviceNum);
+      for (c10::DeviceIndex idx = 0; idx < deviceNum; ++idx) {
+        streams_.emplace_back(at::cuda::getStreamFromPool(
+          /* isHighPriority */ false, /* device */ idx));
+      }
+    }
+  }
+
+  // call c10::cuda::CUDACachingAllocator::recordStream with given DataPtrs
+  void recordDataPtrs(const std::vector<c10::DataPtr>& dataPtrs) const override;
+
+  // call c10::cuda::CUDACachingAllocator::recordStream with given Tensors
+  void recordTensors(const std::vector<torch::Tensor>& tensors) const override;
+
+  // let current streams wait for streams in this context.
+  void blockCurrentStreams() const override;
+
+  // let streams in this context wiat for current streams.
+  void waitForCurrentStreams() const override;
+  const std::vector<CUDAStream>& streams() const override;
+
+ private:
+  std::vector<CUDAStream> streams_;
+};
+
+inline std::shared_ptr<FullDeviceContext> createFullDeviceContext(bool noCuda) {
+  return std::make_shared<CudaFullDeviceContext>(noCuda);
+}
+
+#endif
 
 // A struct that holds pointers that keep alive all the memory that will be
 // accessed by TensorPipe during a write operation.
@@ -43,14 +130,19 @@ struct TensorpipeReadBuffers {
 TORCH_API std::tuple<tensorpipe::Message, TensorpipeWriteBuffers>
 tensorpipeSerialize(
     Message&& rpcMessage,
-    std::vector<c10::DeviceIndex> devices = {});
+    std::vector<c10::DeviceIndex> devices = {},
+    const std::shared_ptr<FullDeviceContext>& =
+        std::make_shared<FullDeviceContext>(/* noCuda */ true));
 
 // Allocate the buffers that will hold the incoming data. They will be managed
 // by the returned holder, which must be kept alive until the asynchronous read
 // has finished. Pointers to these buffers will be stored in-place in the
 // TensorPipe message.
 TORCH_API TensorpipeReadBuffers
-tensorpipeAllocate(tensorpipe::Message& tpMessage);
+tensorpipeAllocate(
+    tensorpipe::Message& tpMessage,
+    const std::shared_ptr<FullDeviceContext>& ctx =
+        std::make_shared<FullDeviceContext>(/* noCuda */ true));
 
 // Convert a TensorPipe message back into an RPC message. This requires the data
 // to be available and can thus only be performed once the asynchronous read has
